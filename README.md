279fd008a3e3ffc86b098db808e4190720e229af
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-04-06 09:13:21 +1200

    Initial prototype

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# venia-ui-resolver-override
+# fooman/venia-ui-override-resolver
+
+`
+yarn add fooman/venia-ui-override-resolver
+`
+
+Then copy individual files from `node_modules/@magento/venia-ui/lib` into `src/overrides/` with the same file path. Please note that adding a new file requires restarting the watch process for the file to get recognised.
---
+++ intercept.js
@@ -0,0 +1,39 @@
+const name = 'fooman/venia-ui-override-resolver';
+const VeniaUiResolverPlugin = require('lib/VeniaUiResolverPlugin');
+const path = require('path');
+const {cachedCleverMerge} = require('webpack/lib/util/cleverMerge');
+
+const customStoreDir = path.resolve(__dirname, '..', '..', '..', 'src', 'overrides');
+const veniaUiModulePath = path.resolve(__dirname, '..', '..', '..', 'node_modules', '@magento', 'venia-ui', 'lib');
+
+const myResolverPlugin = new VeniaUiResolverPlugin({
+    name: name,
+    projectPath: customStoreDir,
+    veniaUiModulePath: veniaUiModulePath
+});
+
+module.exports = targets => {
+    const webpackCompiler = targets.of('@magento/pwa-buildpack').webpackCompiler;
+    webpackCompiler.tap(compiler =>
+        compiler.resolverFactory.hooks.resolveOptions
+            .for('normal')
+            .tap('AddVeniaResolverToWebpackConfig', resolveOptions => {
+                const plugin = Object.assign(
+                    {
+                        plugins: [myResolverPlugin]
+                    });
+                return cachedCleverMerge(plugin, resolveOptions);
+            })
+    );
+    webpackCompiler.tap(compiler =>
+        compiler.resolverFactory.hooks.resolveOptions
+            .for('context')
+            .tap('AddVeniaResolverToWebpackConfig', resolveOptions => {
+                const plugin = Object.assign(
+                    {
+                        plugins: [myResolverPlugin]
+                    });
+                return cachedCleverMerge(plugin, resolveOptions);
+            })
+    );
+};
